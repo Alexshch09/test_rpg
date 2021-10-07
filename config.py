c4cb66646ed86c34acd1ad2e3f7a23d78514721c
Alexshch09 <91371397+Alexshch09@users.noreply.github.com>
2021-10-07 21:02:40 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,4 @@
+WIDTH = 512
+HEIGHT = 512
+FPS = 60
+game_name="Undless Rpg"
---
+++ main.py
@@ -0,0 +1,138 @@
+import pygame
+import time
+from sprites import *
+from config import *
+from colors import *
+from data import *
+from maps import *
+
+x=0
+y=0
+current_map_id=0
+map_list = [
+[start_village, start_village_tiles],
+[forest1, forest1_tiles]
+]
+collide_list=[1,3]
+
+# Создаем игру и окно
+pygame.init()
+pygame.font.init()
+pygame.mixer.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption(game_name)
+clock = pygame.time.Clock()
+all_sprites = pygame.sprite.Group()
+player = Player()
+all_sprites.add(player)
+
+def draw_map(id):
+    map=map_list[id][0]
+    tiles=map_list[id][1]
+    for x in range(8):
+        for y in range(8):
+            sprite_to_draw=tiles[map[y][x]]
+            screen.blit(sprite_to_draw, sprite_to_draw.get_rect(bottomright=(64+x*64, 64+y*64)))
+
+def get_tile_id():
+    id=current_map_id
+    map=map_list[id][0]
+    tiles=map_list[id][1]
+    p_x=player.rect.x/64
+    p_y=player.rect.y/64
+    return map[int(p_y)][int(p_x)]
+
+def tile_func(id):
+    global current_map_id
+    if (id==2):
+        player.rect.x=64
+        player.rect.y=384
+        current_map_id=1
+
+def collide(id):
+    id+=1
+    id2=current_map_id
+    map=map_list[id2][0]
+    if map[id][0] in collide_list:
+        print("Yes!!!")
+    else:
+        print("No")
+
+# Цикл игры
+running = True
+while running:
+    clock.tick(FPS)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]:
+        if (player.rect.x>0):
+            if (player.rect.y<448):
+                id = get_tile_id()
+                p_x=player.rect.x/64
+                p_y=player.rect.y/64
+                map=map_list[current_map_id][0]
+                if map[int(p_y)][int(p_x)-1] in collide_list:
+                    pass
+                else:
+                    for a in range(64):
+                        draw_map(current_map_id)
+                        player.rect.x -= 1
+                        all_sprites.draw(screen)
+                        pygame.display.flip()
+                        time.sleep(0.01)
+    elif keys[pygame.K_RIGHT]:
+        if (player.rect.x<448):
+            if (player.rect.y<448):
+                id = get_tile_id()
+                p_x=player.rect.x/64
+                p_y=player.rect.y/64
+                map=map_list[current_map_id][0]
+                if map[int(p_y)][int(p_x)+1] in collide_list:
+                    pass
+                else:
+                    for a in range(64):
+                        draw_map(current_map_id)
+                        player.rect.x += 1
+                        all_sprites.draw(screen)
+                        pygame.display.flip()
+                        time.sleep(0.01)
+    elif keys[pygame.K_DOWN]:
+        if (player.rect.y<448):
+            id = get_tile_id()
+            p_x=player.rect.x/64
+            p_y=player.rect.y/64
+            map=map_list[current_map_id][0]
+            if map[int(p_y)+1][int(p_x)] in collide_list:
+                pass
+            else:
+                for a in range(64):
+                    draw_map(current_map_id)
+                    player.rect.y += 1
+                    all_sprites.draw(screen)
+                    pygame.display.flip()
+                    time.sleep(0.01)
+    elif keys[pygame.K_UP]:
+        if (player.rect.y>0):
+            id = get_tile_id()
+            p_x=player.rect.x/64
+            p_y=player.rect.y/64
+            map=map_list[current_map_id][0]
+            if map[int(p_y)-1][int(p_x)] in collide_list:
+                pass
+            else:
+                for a in range(64):
+                    draw_map(current_map_id)
+                    player.rect.y -= 1
+                    all_sprites.draw(screen)
+                    pygame.display.flip()
+                    time.sleep(0.01)
+    tile_func(get_tile_id())
+    screen.fill(BLACK)
+    draw_map(current_map_id)
+    all_sprites.draw(screen)
+    pygame.display.flip()
+
+pygame.quit()
