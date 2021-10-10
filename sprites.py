c6b169b807e2fbd8a8cc0cdefb2847ea988117c1
Alexshch09 <91371397+Alexshch09@users.noreply.github.com>
2021-10-10 20:31:22 +0200

    Delete sprites.py

--- sprites.py
+++
@@ -1,5 +0,0 @@
-import pygame
-grass = pygame.image.load('sprites/mine.png')
-player = pygame.image.load('sprites/player.png')
-tree = pygame.image.load('sprites/tree.png')
-exit1 = pygame.image.load('sprites/exit1.png')
